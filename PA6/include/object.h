f6a7eafecf01c60df78277afe136f6bb5251b4c3
Husain Tazarvi <htazarvi@nevada.unr.edu>
2016-10-06 16:19:26 -0700

    Init

---
+++ PA6/include/object.h
@@ -0,0 +1,40 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    Object(const char *objPath);
+    ~Object();
+
+    void Render();
+    glm::mat4 GetModel();
+
+    void Update(unsigned int dt, EventFlag e_flags);
+    void Set_RotateSpeed(float r_speed); 
+    void Set_Scale( float sclr );
+    bool Model_Loader(const char *filePath);
+
+  private:
+    glm::mat4 model;
+    glm::mat4 rotation;
+    glm::mat4 scale;
+
+    float angle_rotate;
+    float rotate_speed;
+    float scaler;
+
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+   
+};
+
+#endif /* OBJECT_H */
---
+++ PA6/src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "engine.h"
+
+
+int main(int argc, char **argv)
+{
+
+  /******************************************/
+
+  /* Check command line arguments */
+  if( argc < 4 ){
+    printf( "ERROR: Incorrect command line arguments (MISSING ARGUMENT)\n" );
+    printf("\n Usage : <exe-filename> <vert-shader-filepath> <frag-shader-filepath> <obj-filepath>\n");
+    printf("\n Example : ./Tutorial ../files/vShader.vert ../files/fShader.frag ../models/box.obj\n");
+    return 1;
+  }
+  if( argc > 4 ){
+    printf( "ERROR: Incorrect command line arguments (TOO MANY ARGUMENTS)\n" );
+    printf("\n Usage : <exe-filename> <vert-shader-filepath> <frag-shader-filepath> <obj-filepath>\n");
+    printf("\n Example : ./Tutorial ../files/vShader.vert ../files/fShader.frag ../models/box.obj\n");
+    return 1;
+  }
+
+  /******************************************/
+
+  // Start an engine and run it then cleanup after
+  Engine *engine = new Engine("Tutorial Window Name", 800, 600);
+  if(!engine->Initialize(argv))
+  {
+    printf("The engine failed to start.\n");
+    delete engine;
+    engine = NULL;
+    return 1;
+  }
+  engine->Run();
+  delete engine;
+  engine = NULL;
+  return 0;
+}
