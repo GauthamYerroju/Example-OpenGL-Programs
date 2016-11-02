2d0bdd92b26e7a896f8e0d83c7a5ae7751f4e857
Gautham Yerroju <gautham.yerroju@gmail.com>
2016-11-01 19:16:09 -0700

    Functions for ray casting (not working).

--- PA8/include/camera.hpp
+++ PA8/include/camera.hpp
@@ -11,10 +11,15 @@ class Camera
     bool Initialize(int w, int h);
     glm::mat4 GetProjection();
     glm::mat4 GetView();
+    glm::vec3 GetPosition();
+    glm::vec3 RayCast(int x, int y);
 
   private:
     glm::mat4 projection;
     glm::mat4 view;
+    glm::vec3 position;
+    int width;
+    int height;
 };
 
 #endif /* CAMERA_H */
